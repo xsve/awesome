dd10c56af9164e82bda774dceeff55bb17056697
Sergei Velhas <sergei@velhas.net>
2014-08-20 19:38:14 +0300

    [FRESH Install] logs

---
+++ freshinstall.sh
@@ -0,0 +1,36 @@
+sudo apt-get install aptitude
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java7-installer
+sudo apt-get install htop
+sudo apt-get install oracle-java7-set-default
+sudo apt-get install lynx
+sudo apt-get install pidgin-dev
+sudo apt-get install pidgin-dev pidgin-skype rdesktop kompare
+sudo apt-get install pidgin-dev pidgin-skype rdesktop 
+sudo apt-get install jmeter graphviz gummi
+sudo apt-get install jmeter graphviz 
+sudo add-apt-repository ppa:keks9n/skypetab
+sudo apt-get update
+sudo apt-get install skypetab-ng
+sudo apt-get update
+sudo apt-get install skype
+sudo add-apt-repository ppa:cwchien/gradle
+sudo apt-get install gradle
+sudo apt-get install rar
+sudo apt-get install unrar
+sudo apt-get install screen
+sudo apt-get install git zsh tig 
+sudo apt-get install awesome
+sudo add-apt-repository "deb http://archive.canonical.com/ $(lsb_release -sc) partner"
+sudo apt-get install nautilus
+sudo apt-get install wireshark
+sudo apt-get install gitk
+sudo apt-get install git
+sudo apt-get install libreoffice
+sudo apt-get install urxvt
+sudo apt-get install vim
+sudo apt-get install git-cola
+sudo chsh -s $(which zsh) $USER 
+sudo apt-get install gnome-terminal
+killall xfwm4 && awesome
